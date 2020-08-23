220ced1b5a2ad55075cbc1e02950239f145dd072
Matt <matt@deity.co.nz>
2020-08-23 16:54:52 +1200

    Preliminary tidying Image blending Image stitching Logging

---
+++ Funhouse.Test/EndToEndTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Funhouse.Test
+{
+    [TestFixture(TestOf = typeof(Funhouse))]
+    public class EndToEndTests
+    {
+        [Test]
+        // TODO make this a proper test plz
+        public void Stitched()
+        {
+            Funhouse.Main( 
+                "-o", "out.jpg", 
+                "-s", "foo",
+                "--stitch",
+                "--autocrop");
+         
+            Assert.True(true, "Write me");
+        }
+    }
+}
---
+++ Funhouse.Test/Helper/FileHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Funhouse.Test.Helper
+{
+    /// <summary>
+    ///     Helper methods for file manipulation and cleanup in unit tests
+    /// </summary>
+    public static class FileHelper
+    {
+        public static FileState NewState()
+        {
+            return new FileState();
+        }
+    }
+
+    public class FileState : IDisposable
+    {
+        private readonly List<string> _tempDirectories = new List<string>();
+
+        internal FileState()
+        {
+        }
+
+        public string CreateFile(string filename)
+        {
+            var tempDirectory = CreateTempDirectory();
+            var filePath = Path.Combine(tempDirectory, filename);
+
+            using var stream = File.Create(filePath);
+            return stream.Name;
+        }
+
+        public string CreateTempDirectory()
+        {
+            var path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(path);
+
+            _tempDirectories.Add(path);
+
+            return path;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+
+            foreach (var path in _tempDirectories)
+                if (Directory.Exists(path))
+                    Directory.Delete(path, true);
+        }
+    }
+}
