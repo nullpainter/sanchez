76cc4a3c8ed14b299b58b15397fadd2062dc1411
Matt <matt@deity.co.nz>
2020-09-20 09:39:10 +1200

    Remove commented crop code

--- Sanchez/Models/CommandLine/EquirectangularOptions.cs
+++ Sanchez/Models/CommandLine/EquirectangularOptions.cs
@@ -8,9 +8,6 @@ namespace Sanchez.Models.CommandLine
     {
         [Option('a', "autocrop", HelpText = "Whether to create an automatically cropped image. Only applicable when stitching.", Required = false, Default = false)]
         public bool AutoCrop { get; [UsedImplicitly] set; }
-
-        // [Option('e', "extents", HelpText = "Latitude/longitude extents. Format is minlat:maxlat:minlon:maxlon; e.g. -100:100:-45:45", Required = false)]
-        // public string? Extents { get; [UsedImplicitly] set; }
         
         [Option('m', "mode", HelpText = "Whether source images are stitched together or rendered individually in a batch", Default = EquirectangularMode.Batch)]
         public EquirectangularMode Mode { get; [UsedImplicitly] set; }
