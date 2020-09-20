b45b719f3ebdc3014866556210d5854ec282f8b4
Matt <matt@deity.co.nz>
2020-09-20 19:36:14 +1200

    Relax GK-2A filename prefix parsing

--- Tests/Sanchez.Test/Filesystem/Gk2aFilenameParserTests.cs
+++ Tests/Sanchez.Test/Filesystem/Gk2aFilenameParserTests.cs
@@ -9,27 +9,26 @@ namespace Sanchez.Test.Filesystem
     [TestFixture(TestOf = typeof(Gk2AFilenameParser))]
     public class Gk2AFilenameParserTests : AbstractTests
     {
-        private IFilenameParser FilenameParser = new Gk2AFilenameParser();
+        private readonly IFilenameParser _filenameParser = new Gk2AFilenameParser();
 
         [Test]
         public void ExtractDate()
         {
-            var date = FilenameParser.GetTimestamp("IMG_FD_020_IR105_20190907_032006.jpg");
+            var date = _filenameParser.GetTimestamp("IMG_FD_020_IR105_20190907_032006.jpg");
             date.Should().Be(new DateTime(2019, 09, 07, 03, 20, 06));
-
         }
 
         [Test]
         public void MissingDate()
         {
-            var date = FilenameParser.GetTimestamp("GOES17_FD_CH13_20200830T033031Z.jpg");
+            var date = _filenameParser.GetTimestamp("GOES17_FD_CH13_20200830T033031Z.jpg");
             date.Should().BeNull();
         }
 
         [Test]
         public void Suffix()
         {
-            var date = FilenameParser.GetTimestamp("IMG_FD_020_IR105_20190907_032006_ENHANCED.jpg");
+            var date = _filenameParser.GetTimestamp("IMG_FD_020_IR105_20190907_032006_ENHANCED.jpg");
             date.Should().BeNull();
         }
     }
