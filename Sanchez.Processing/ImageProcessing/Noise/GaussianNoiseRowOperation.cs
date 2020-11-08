a0d9039d2251df11952b7775cd579380ffa7e449
Matt Painter <matthewpainter@co-operativebank.co.nz>
2020-11-08 21:21:09 +1300

    Add gaussian noise extension

---
+++ Sanchez.Processing/ImageProcessing/Noise/GaussianNoiseRowOperation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Sanchez.Processing.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Sanchez.Processing.ImageProcessing.Noise
+{
+    public struct GaussianNoiseRowOperation : IRowOperation
+    {
+        private readonly Image<Rgba32> _image;
+
+        private const int Mean = 0;
+        private const int StdDev = 1;
+
+        public GaussianNoiseRowOperation(Image<Rgba32> image) => _image = image;
+
+        public void Invoke(int y)
+        {
+            var random = new Random();
+
+            var span = _image.GetPixelRowSpan(y);
+
+            for (var x = 0; x < span.Length; x++)
+            {
+                var u1 = 1.0 - random.NextDouble();
+                var u2 = 1.0 - random.NextDouble();
+
+                var randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(Constants.Pi2 * u2);
+
+                var randNormal = (float) (Mean + StdDev * randStdNormal);
+
+                span[x] = new Rgba32(randNormal, randNormal, randNormal, 1); //span[x].A);
+            }
+        }
+    }
+}
---
+++ Sanchez.Processing/ImageProcessing/Noise/NoiseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace Sanchez.Processing.ImageProcessing.Noise
+{
+    public static class NoiseExtensions
+    {
+        public static void ApplyGaussianNoise(this Image<Rgba32> image)
+        {
+            var noiseImage = image.Clone();
+
+            var operation = new GaussianNoiseRowOperation(noiseImage);
+            ParallelRowIterator.IterateRows(Configuration.Default, noiseImage.Bounds(), in operation);
+            
+            image.Mutate(c => c.DrawImage(noiseImage, PixelColorBlendingMode.Multiply, 0.1f));
+        }
+    }
+}
