7ecacfd7cc5c260a82a537c170f3218e96a8d6db
Matt Painter <matthewpainter@co-operativebank.co.nz>
2020-12-03 12:17:47 +1300

    Comments

--- Sanchez.Workflow/Steps/Equirectangular/OffsetImage.cs
+++ Sanchez.Workflow/Steps/Equirectangular/OffsetImage.cs
@@ -1,4 +1,3 @@
-using System;
 using Ardalis.GuardClauses;
 using JetBrains.Annotations;
 using Sanchez.Processing.Extensions;
@@ -14,7 +13,10 @@ using WorkflowCore.Models;
 
 namespace Sanchez.Workflow.Steps.Equirectangular
 {
-    // TODO comment
+    /// <summary>
+    ///     Horizontally offsets and wraps an equirectangular projected image by <see cref="GlobalOffset"/> radians. This is
+    ///     performed to ensure that visible satellite imagery is contiguous.
+    /// </summary>
     internal sealed class OffsetImage : StepBody, IActivityStepBody
     {
         private readonly RenderOptions _options;
@@ -29,21 +31,24 @@ namespace Sanchez.Workflow.Steps.Equirectangular
             Guard.Against.Null(TargetImage, nameof(TargetImage));
             Guard.Against.Null(Activity, nameof(Activity));
 
-            // TODO create test case for this
+            // No offset required if full earth coverage and no explicit crop is being performed
             if (Activity.IsFullEarthCoverage() && !_options.EquirectangularRender!.ExplicitCrop) return ExecutionResult.Next();
-            
+
             var offset = GlobalOffset
                 .NormaliseLongitude()
                 .ToX(TargetImage.Width);
-            
+
             TargetImage.HorizontalOffset(offset);
-            
+
             return ExecutionResult.Next();
         }
     }
-    
+
     internal static class OffsetImageExtensions
     {
+        /// <summary>
+        ///     Horizontally offsets and wraps an equirectangular projected image by <see cref="StitchWorkflowData.GlobalOffset"/> radians. 
+        /// </summary>
         internal static IStepBuilder<TData, OffsetImage> OffsetImage<TStep, TData>(this IStepBuilder<TData, TStep> builder)
             where TStep : IStepBody
             where TData : StitchWorkflowData
