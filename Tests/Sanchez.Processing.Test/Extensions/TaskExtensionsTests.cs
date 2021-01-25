81892c6627258ffaefb78d06dba70439743eddcd
Matt <matt@deity.co.nz>
2021-01-25 21:37:35 +1300

    .NET 5.0 and C# 9 upgrade

--- Tests/Sanchez.Processing.Test/Extensions/TaskExtensionsTests.cs
+++ Tests/Sanchez.Processing.Test/Extensions/TaskExtensionsTests.cs
@@ -15,7 +15,7 @@ namespace Sanchez.Processing.Test.Extensions
         [Repeat(100)]
         public async Task ForEachAsync()
         {
-            var models = new List<TestModel> { new TestModel(), new TestModel() };
+            var models = new List<TestModel> { new(), new() };
             await models.ForEachAsync(async a => await Task.Run(() => a.TestValue++), 10);
 
             models.ForEach(m => m.TestValue.Should().Be(1, "operation should have been performed on each item"));
