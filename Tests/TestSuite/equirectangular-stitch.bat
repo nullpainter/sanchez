3a68bfb297b7486ac5b2888e37516773a3443270
Matt <matt@deity.co.nz>
2021-02-05 19:41:31 +1300

    Add 0.5km stitch

--- Tests/TestSuite/equirectangular-stitch.bat
+++ Tests/TestSuite/equirectangular-stitch.bat
@@ -1,3 +1,4 @@
+sanchez\sanchez reproject -o output\equirectangular\equirectangular-stitched-0.5.jpg -s sample-images\subset -T 2020-08-30T03:50:20 -f -r 0.5 %1 %2 %3 %4 %5
 sanchez\sanchez reproject -o output\equirectangular\equirectangular-stitched.jpg -s sample-images\subset -T 2020-08-30T03:50:20 -f %1 %2 %3 %4 %5
 sanchez\sanchez reproject -o output\equirectangular\equirectangular-stitched-clut.jpg -s sample-images\subset -T 2020-08-30T03:50:20 -f -c0-1
 sanchez\sanchez reproject -o output\equirectangular\equirectangular-stitched-nocrop.jpg -s sample-images\subset -T 2020-08-30T03:50:20 -f --nocrop
