cb3629075887ebab2ad148f7ab39a3e814b41a92
Matt Painter <matthewpainter@co-operativebank.co.nz>
2021-02-18 19:48:37 +1300

    Add support for non-UTC filename timestamps

--- Sanchez.Processing/Services/Filesystem/Parsers/ElectroFilenameParser.cs
+++ Sanchez.Processing/Services/Filesystem/Parsers/ElectroFilenameParser.cs
@@ -1,14 +1,17 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Sanchez.Processing.Services.Filesystem.Parsers
 {
     /// <summary>
-    ///     Parses filenames of satellite imagery received from Electro-L no. 2.
+    ///     Parses filenames of satellite imagery received from Electro-L no. 2. Note that Electro timestamps
+    ///     are in Russian Standard Time, not UTC.
     /// </summary>
     public class ElectroFilenameParser : AbstractFilenameParser
     {
         protected override Regex Regex { get; }
         protected override string TimestampFormat => "yyMMdd_HHmm";
+        protected override TimeZoneInfo TimeZone => TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
 
         public ElectroFilenameParser(string? prefix, string? suffix) 
         {
