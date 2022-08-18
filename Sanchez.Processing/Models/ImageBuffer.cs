e69749101e447b385014ee7c37e88bed48b53b05
Matt <matt@deity.co.nz>
2022-08-18 20:31:33 +1200

    Simplify buffer

--- Sanchez.Processing/Models/ImageBuffer.cs
+++ Sanchez.Processing/Models/ImageBuffer.cs
@@ -19,27 +19,9 @@ public class ImageBuffer
 
     private static Rgba32[] GetBuffer(Image<Rgba32> image)
     {
-        // Return single span if present
-        if (image.DangerousTryGetSinglePixelMemory(out var pixelSpan))
-        {
-            return pixelSpan.ToArray();
-        }
-
-        var memoryGroup = image.GetPixelMemoryGroup();
-
-        // Combine spans for larger images
-        var bufferSize = memoryGroup.Select(g => g.Span.Length).Sum();
-        var buffer = new Rgba32[bufferSize];
-
-        var offset = 0;
-        foreach (var group in memoryGroup)
-        {
-            var span = group.Span;
-            Array.Copy(span.ToArray(), 0, buffer, offset, span.Length);
-
-            offset += span.Length;
-        }
-
+        var buffer = new Rgba32[image.Width * image.Height];
+        image.CopyPixelDataTo(buffer);
+     
         return buffer;
     }
 }
