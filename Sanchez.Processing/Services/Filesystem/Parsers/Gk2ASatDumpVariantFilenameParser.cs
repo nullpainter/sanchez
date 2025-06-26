0b6d5b7cdbc0e8e74cbd8aefc1dce82fa8a44f78
Matt Painter <matt@deity.co.nz>
2025-06-26 20:26:37 +1200

    Tidy

--- Sanchez.Processing/Services/Filesystem/Parsers/Gk2ASatDumpVariantFilenameParser.cs
+++ Sanchez.Processing/Services/Filesystem/Parsers/Gk2ASatDumpVariantFilenameParser.cs
@@ -5,14 +5,9 @@ namespace Sanchez.Processing.Services.Filesystem.Parsers;
 /// <summary>
 ///     Parses filenames of satellite imagery received from GK-2A and processed by <c>xrit-rc</c>.
 /// </summary>
-public class Gk2ASatDumpVariantFilenameParser : AbstractFilenameParser
+public class Gk2ASatDumpVariantFilenameParser(string? prefix, string? suffix) : AbstractFilenameParser
 {
-    protected override Regex Regex { get; }
+    protected override Regex Regex { get; } = new(prefix + "([0-9]{8}T[0-9]{6}Z)" + suffix + "\\.[^ ]*", RegexOptions.Compiled);
 
     protected override string TimestampFormat => "yyyyMMddTHHmmssZ";
-
-    public Gk2ASatDumpVariantFilenameParser(string? prefix, string? suffix)
-    {
-        Regex = new Regex(prefix + "([0-9]{8}T[0-9]{6}Z)" + suffix + "\\.[^ ]*", RegexOptions.Compiled);
-    }
 }
